20a07b27dd5e358a12a7e595d74f904b15b864fa
resoliwan <resoliwan@naver.com>
2017-11-14 13:07:16 +0900

    The ansewer for police iteration is only for determistic policy but I implement for stocastic policy

--- DP/PolicyIteration.py
+++ DP/PolicyIteration.py
@@ -40,7 +40,7 @@ def policy_eval(policy, env, discount_factor=1.0, theta=0.00001):
 # random_policy = np.ones([env.nS, env.nA]) / env.nA
 # v = policy_eval(random_policy, env)
 # print('v', v)
-
+#
 # expected_v = np.array([0, -14, -20, -22, -14, -18, -20, -20, -20, -20, -18, -14, -22, -20, -14, 0])
 # np.testing.assert_array_almost_equal(v, expected_v, decimal=2)
 
@@ -80,18 +80,35 @@ def policy_improvment(env, policy_eval_fn=policy_eval, discount_factor=1.0):
                 qs.append(qvalue)
 
             optimal_actions = np.array(qs) >= max_qvalue
-            policy[s] = optimal_actions / env.nA
+            new_policy = optimal_actions / env.nA
 
-            if not np.array_equal(old_policy, policy[s]):
+            if not np.array_equal(old_policy, new_policy):
+                policy[s] = new_policy
                 isChanged = True
 
         if isChanged == False:
             break
+    
+        print('V', V)
+        
 
     return policy, V
-
 policy, v = policy_improvment(env)
-print('policy', policy)
+print("Policy Probability Distribution:")
+print(policy)
+print("")
+
+print("Reshaped Grid Policy (0=up, 1=right, 2=down, 3=left):")
+print(np.reshape(np.argmax(policy, axis=1), env.shape))
+print("")
+
+print("Value Function:")
+print(v)
+print("")
+
+print("Reshaped Grid Value Function:")
+print(v.reshape(env.shape))
+print("")
 
 expected_v = np.array([ 0, -1, -2, -3, -1, -2, -3, -2, -2, -3, -2, -1, -3, -2, -1,  0])
 np.testing.assert_array_almost_equal(v, expected_v, decimal=2)
